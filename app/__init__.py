37eaaf9d4f88b0f7f06e5850bc79f90dcbdf4f23
qf <qf0129@qq.com>
2018-05-06 03:28:24 +0800

    save

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
 
 db = SQLAlchemy()
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
     _init_config(app)
     _init_db(app)
     _init_api(app)
+    _init_login(app)
     return app
 
 def _init_config(app):
@@ -17,8 +21,24 @@ def _init_config(app):
 def _init_db(app):
     db.init_app(app)
 
+
 def _init_api(app):
     from app.api import init_api
     init_api(app)
+
+def _init_login(app):
+    login_manager.init_app(app)
+
+    from flask import request, redirect
+    from urllib.parse import quote
+    
+    def unauthorized():
+        return redirect('/login?return=%s' % (quote(request.url)))
+    login_manager.unauthorized = unauthorized
+
+    from app.models import User
+    @login_manager.user_loader
+    def load_user(userid):
+        return User.query.get(userid)
 
 app = create_app()
--- app/config.py
+++ app/config.py
@@ -1,4 +1,13 @@
+import os
+
+
 class BaseConfig(object):
+    DEBUG = True
+    SECRET_KEY = "SECRET_KEY"
+
+    APP_DIR = os.path.dirname(__file__)
+    ROOT_DIR = os.path.dirname(APP_DIR)
+    VAR_DIR = ROOT_DIR + '/var/'
+
     SQLALCHEMY_DATABASE_URI = 'sqlite:///../test.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG = True
